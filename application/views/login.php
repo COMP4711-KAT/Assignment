be58bede910d8eb982dd4a736d4c986cf686fad6
TrevorBroderick <Broderick.Trevor@gmail.com>
2016-04-14 09:20:51 -0700

    Prettyfied the login and register pages with bootstrap

--- application/views/login.php
+++ application/views/login.php
@@ -1,7 +1,15 @@
-<?php echo form_open('portfolio/verify_login'); ?>
-  <label for="UserId">Username:</label>
-  <input type="text" size="20" id="UserId" name="UserId"/><br/>
-  <label for="Password">Password:</label>
-  <input type="password" size="32" name="Password" /><br/>
-  <input type="submit" value="Login"/>
-</form>
+<div class="row">
+    <div class="col-md-4">
+        <?php echo form_open('portfolio/verify_login'); ?>
+            <div class="form-group">
+                <label for="UserId">Username:</label>
+                <input class="form-control" type="text" id="UserId" name="UserId"/><br/>
+            </div>
+            <div class="form-group">
+                <label for="Password">Password:</label>
+                <input class="form-control" type="password" name="Password" /><br/>
+            </div>
+            <button class="btn btn-primary" type="submit">Submit</button>
+        </form>
+    </div>
+</div>

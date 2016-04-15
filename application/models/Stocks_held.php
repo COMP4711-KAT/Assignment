1b8d9b3cb96e2740a8b4d67ddc7bad249135946a
TrevorBroderick <Broderick.Trevor@gmail.com>
2016-04-14 20:08:18 -0700

    Buying and selling work flawlessly now.

--- application/models/Stocks_held.php
+++ application/models/Stocks_held.php
@@ -36,4 +36,43 @@ class Stocks_held extends MY_Model {
 
         return $certificates;
     }
+
+    /**
+     * Updates the certificates for a stock in the database by deleting the existing ones and inserting
+     * a new record.
+     *
+     * @param $agent string agent name
+     * @param $player string player name
+     * @param $stock string stock code
+     * @param $quantity the amount of remaining stock
+     * @param $certificate string the certificate for the remaining stock
+     */
+    function update_certificates($agent, $player, $stock, $quantity, $certificate) {
+        // Delete the exiting records
+         $this->delete_certificates($agent, $player, $stock);
+
+        // Insert a new record
+        $stock = array('agent' => $agent, 'player' => $player,
+                       'stock' => $stock, 'amount' => $quantity,
+                       'token' => $certificate, 'id' => null,
+                       'datetime' => date());
+
+        $this->db->insert($this->_tableName, $stock);
+    }
+
+    /**
+     * Deletes the certificates for a stock in the database.
+     *
+     * @param $agent string agent name
+     * @param $player string player name
+     * @param $stock string stock code
+     */
+    function delete_certificates($agent, $player, $stock) {
+        // Delete the exiting records
+        $this->db->where('agent', $agent);
+        $this->db->where('player', $player);
+        $this->db->where('stock', $stock);
+
+        $this->db->delete($this->_tableName);
+    }
 }
